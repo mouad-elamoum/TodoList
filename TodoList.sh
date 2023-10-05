d1e13db2654fe23236b466c2820c218d8ae2b6c1
mouad-elamoum <elamoum.mouad@gmail.com>
2023-10-05 15:31:12 +0200

    suppression fichiers

--- TodoList.sh
+++
@@ -1,3 +0,0 @@
-
-echo "Entrez les chiffres correspondant à votre choix"
-read -p "" confirmation
--- todo.sh
+++ todo.sh
@@ -1,3 +1,3 @@
 echo "bienvenue dans la todo list, que voulez vous faire: 1 ajouter une tache  , 2 la supprimer ou 3 afficer les tâches: "
-
+read "Entrez les chiffres correspondant à votre choix"
 echo "hello"
