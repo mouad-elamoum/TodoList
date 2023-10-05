52f8b77d1db01a24e830fe09c92a831a1c6c0109
moussaFer <feraouni.moussa.info@gmail.com>
2023-10-05 16:58:19 +0200

    test du if et affichage de la tache a faire

--- todo.sh
+++ todo.sh
@@ -1,3 +1,11 @@
 echo "bienvenue dans la todo list, que voulez vous faire: 1 ajouter une tache  , 2 la supprimer ou 3 afficer les tâches: "
 
-echo "entrez le nombre correspondant à la tache voulu: "
+read -p "entrez le nombre correspondant à la tache voulu: " nombre
+
+if [ $nombre -eq 1 ]
+then
+    touch tasks.txt
+    read -p "entrez la tache voulu: (exemple faire les courses,etc) " tache
+    echo "a faire: " $tache
+fi
+
