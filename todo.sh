c8148059f3657ca0cadf56a4f6e5c185bf5f2db2
moussaFer <feraouni.moussa.info@gmail.com>
2023-10-06 13:04:11 +0200

    finalisation sans commentaire

--- todo.sh
+++ todo.sh
@@ -1,20 +1,35 @@
-echo "bienvenue dans la todo list, que voulez vous faire: 1 ajouter une tache , 2 la supprimer, 3 afficer les tâches ou 4 revenir au message d'acceuil: "
+echo "=================================================================================================================="
+echo "bienvenue dans la todo list, que voulez vous faire: 1 ajouter une tache | 2 la supprimer | 3 l'afficher  "
+echo "=================================================================================================================="
 
 read -p "entrez le nombre correspondant à la tache voulu: " nombre
 
-while [ $nombre -ne 4 ]
+while [ $nombre -eq 1 ]
+read -p "entrez le nombre correspondant à la tache voulu: " nombre
 do
-read -p "entrez le nombre correspondant à la tache voulu: " nombre
 if [ $nombre -eq 1 ]
 then
     touch tasks.txt
     read -p "entrez la tache voulu: (exemple faire les courses,etc) " tache
     echo "$tache" >> tasks.txt
     nl -ba tasks.txt
-
+    
 elif [ $nombre -eq 2 ]
 then
-    echo "LE NOMBRE VAUT 2"
+    nl -ba tasks.txt
+    read -p "entrez le numéro de la tâche a supprimer : " numTache
+    read -p "entrez le nombre correspondant à la tache voulu: " nombre
+    gsed -i "${numTache}d"  tasks.txt
+    nl -ba tasks.txt
+
+elif [ $nombre -eq 3 ]
+then
+    cat -n tasks.txt
 fi 
 
 done
+
+if [ $nombre -eq 3 ]
+then
+    cat -n tasks.txt
+fi 
