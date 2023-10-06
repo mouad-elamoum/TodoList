c967bb6241c7cd3834bdc271919f3f62e198d3e4
moussaFer <feraouni.moussa.info@gmail.com>
2023-10-06 10:54:43 +0200

    ajout tache et listing

--- todo.sh
+++ todo.sh
@@ -4,16 +4,17 @@ read -p "entrez le nombre correspondant à la tache voulu: " nombre
 
 while [ $nombre -ne 4 ]
 do
+read -p "entrez le nombre correspondant à la tache voulu: " nombre
 if [ $nombre -eq 1 ]
 then
     touch tasks.txt
     read -p "entrez la tache voulu: (exemple faire les courses,etc) " tache
     echo "$tache" >> tasks.txt
-    echo "le contenue de tasks.txt est: " 
-    cat tasks.txt
+    nl -ba tasks.txt
 
-elif [ $nombre -eq 4]
+elif [ $nombre -eq 2 ]
 then
-    echo "bienvenue dans la todo list, que voulez vous faire: 1 ajouter une tache , 2 la supprimer, 3 afficer les tâches ou 4 revenir au message d'acceuil: "
-    read -p "entrez le nombre correspondant à la tache voulu: " nombre
+    echo "LE NOMBRE VAUT 2"
 fi 
+
+done
