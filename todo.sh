d6d1e5fbef7860029995f4ec6e6f2d4f02867dd2
mouad-elamoum <elamoum.mouad@gmail.com>
2023-10-06 11:08:44 +0200

    vérification des conditions

--- todo.sh
+++ todo.sh
@@ -2,18 +2,22 @@ echo "bienvenue dans la todo list, que voulez vous faire: 1 ajouter une tache , 
 
 read -p "entrez le nombre correspondant à la tache voulu: " nombre
 
-while [ $nombre -ne 4 ]
+while [ $nombre -eq 1 ]
 do
-if [ $nombre -eq 1 ]
-then
     touch tasks.txt
     read -p "entrez la tache voulu: (exemple faire les courses,etc) " tache
     echo "$tache" >> tasks.txt
     echo "le contenue de tasks.txt est: " 
     cat tasks.txt
 
-elif [ $nombre -eq 4]
+elif [ $nombre -eq 2 ]
+
+
+
+elif [ $nombre -eq 4 ]
 then
     echo "bienvenue dans la todo list, que voulez vous faire: 1 ajouter une tache , 2 la supprimer, 3 afficer les tâches ou 4 revenir au message d'acceuil: "
     read -p "entrez le nombre correspondant à la tache voulu: " nombre
+
 fi 
+done
